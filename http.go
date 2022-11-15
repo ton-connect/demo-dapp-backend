97ebef3b41bb48e0733bebcc3060d11753d03b94
Maxim Khoroshev <imxoiam1@gmail.com>
2022-11-15 17:42:29 +0700

    fix

--- http.go
+++ http.go
@@ -9,16 +9,17 @@ func registerHandlers(e *echo.Echo, h *handler) {
 	proof := e.Group("/ton-proof")
 	proof.POST("/generatePayload", h.PayloadHandler, middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"POST"},
+		AllowMethods: []string{echo.POST},
 	}))
 	proof.POST("/checkProof", h.ProofHandler, middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"POST"},
+		AllowMethods: []string{echo.POST},
 	}))
 	dapp := e.Group("/dapp")
+	dapp.Use(middleware.CORS())
 	dapp.GET("/getAccountInfo", h.GetAccountInfo, middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"GET"},
+		AllowMethods: []string{echo.GET},
 	}), middleware.JWTWithConfig(middleware.JWTConfig{
 		Claims:     &jwtCustomClaims{},
 		SigningKey: []byte("secret"),
